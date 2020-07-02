b71af8c35ae843b384ac4db1b57c054d6b3f2c47
Vinaysheel Wagh <42643440+vinaysheelwagh@users.noreply.github.com>
2020-07-02 12:00:38 +0100

    Add files via upload

---
+++ Dashboard/dapDash.js
@@ -0,0 +1,9 @@
+function usaClicked(){
+	location.href = "file:///C:/DAPDashboard/Environmental_Factor.html";
+}
+function caClicked(){
+	location.href = "file:///C:/DAPDashboard/California.html";
+}
+function backClicked(){
+	location.href = "file:///C:/DAPDashboard/dapDash.html";
+}
